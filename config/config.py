f877905a9e6b5029af88e081fc85a97b7760ed88
sachinopboy2 <sachin25u7@gmail.com>
2025-04-07 14:37:24 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -73,7 +73,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vishalpandeynkp/privete-nhi-h-bhai",
+    "https://github.com/sachinopboy2/professor-repo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
